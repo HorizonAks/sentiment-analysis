c144cf5a47adbd550afa57a173993844fd04cf34
Akshat Jain <36150234+HorizonAks@users.noreply.github.com>
2022-03-24 18:34:34 +0530

    Add Azure deployment files

--- SAfront/settings.py
+++ SAfront/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-59v09v06s-qmg=t7z!z9g9$+2bv*hcf9b^^7+b66mb8ug(bkf=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
